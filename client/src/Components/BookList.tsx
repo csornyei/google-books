82c2d15ea955ce8399ea2728f5955e68adfeff6e
Mate Mark Csornyei <mate@csornyei.com>
2023-02-06 20:03:55 +0100

    BookList mobile view

--- client/src/Components/BookList.tsx
+++ client/src/Components/BookList.tsx
@@ -17,9 +17,16 @@ const BookList: React.FC<BookListProps> = ({ books }) => {
   return (
     <ul className="flex flex-col md:flex-row md:flex-wrap">
       {books.map((book) => (
-        <li key={book.id} className="rounded text-center md:w-1/4">
-          <img src={book.thumbnail} alt={book.title} className="mx-auto h-48" />
-          <div className="px-4 pt-2 pb-8">
+        <li
+          key={book.id}
+          className="flex flex-row rounded px-4 pb-4 text-center md:w-1/4 md:flex-col md:px-0"
+        >
+          <img
+            src={book.thumbnail}
+            alt={book.title}
+            className="mx-auto h-48 w-32"
+          />
+          <div className="flex-grow px-4 pt-2 pb-8 md:flex-grow-0">
             <h3 className="font-bold">{book.title}</h3>
             <p>{book.authors.join(", ")}</p>
             <p className="mb-4 text-sm font-thin text-slate-600">
